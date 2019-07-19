0b94c450e5efc1429f2087d5c0ae6fc1b0d6b0b4
liuzewei <1433427550@qq.com>
2019-07-19 19:58:35 +0800

    wu

---
+++ src/main/java/com/liu/community/util/MailClient.java
@@ -0,0 +1,14 @@
+package com.liu.community.util;
+
+/**
+ * @ProjectName: community
+ * @Package: com.liu.community.util
+ * @ClassName: mail
+ * @Author: liuze
+ * @Description: ${description}
+ * @Date: 2019/7/19 19:56
+ * @Version: 1.0
+ */
+public class MailClient {
+    
+}
