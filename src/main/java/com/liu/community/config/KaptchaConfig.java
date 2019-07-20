f9f66404a2b89367f29e8fd15f0a97b7cca59102
liuzewei <1433427550@qq.com>
2019-07-20 15:21:24 +0800

    wu

---
+++ src/main/java/com/liu/community/config/KaptchaConfig.java
@@ -0,0 +1,31 @@
+package com.liu.community.config;
+
+import com.google.code.kaptcha.Producer;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration
+public class KaptchaConfig {
+//生成验证码的工具配置类(由于Spring没有为kaptcha提供自动配置)
+    @Bean//此Bean将会被Spring容器所管理，装配
+    public Producer kaptchaProducer() {
+        Properties properties = new Properties();
+        properties.setProperty("kaptcha.image.width", "100");
+        properties.setProperty("kaptcha.image.height", "40");
+        properties.setProperty("kaptcha.textproducer.font.size", "32");
+        properties.setProperty("kaptcha.textproducer.font.color", "0,0,0");
+        properties.setProperty("kaptcha.textproducer.char.string", "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYAZ");
+        properties.setProperty("kaptcha.textproducer.char.length", "4");
+        properties.setProperty("kaptcha.noise.impl", "com.google.code.kaptcha.impl.NoNoise");
+
+        DefaultKaptcha kaptcha = new DefaultKaptcha();
+        Config config = new Config(properties);
+        kaptcha.setConfig(config);
+        return kaptcha;
+    }
+
+}
---
+++ src/main/java/com/liu/community/dao/LoginTicketMapper.java
@@ -0,0 +1,41 @@
+package com.liu.community.dao;
+
+import com.liu.community.entity.LoginTicket;
+import org.apache.ibatis.annotations.*;
+
+/**
+ * @ProjectName: community
+ * @Package: com.liu.community.dao
+ * @ClassName: LoginTicketMapper
+ * @Author: liuze
+ * @Description: ${description}
+ * @Date: 2019/7/20 15:07
+ * @Version: 1.0
+ */
+@Mapper
+public interface LoginTicketMapper {
+
+    @Insert({
+    "insert into login_ticket(user_id,ticket,status,expired)",
+            "values(#{userId},#{ticket},#{status},#{expired})"
+    })
+    @Options(useGeneratedKeys = true,keyProperty = "id")
+    int insertLoginTicket(LoginTicket loginTicket);
+
+    @Select({
+        "select id,user_id,ticket,status,expired","from login_ticket where ticket=#{ticket}"
+    })
+    LoginTicket selectByTicket(String ticket);
+
+    @Update({
+            "<script>",
+            "update login_ticket set status=#{status} where ticket=#{ticket} ",
+            "<if test=\"ticket!=null\"> ",
+            "and 1=1 ",
+            "</if>",
+            "</script>"
+    })
+    int updateStatus(String ticket, int status);
+
+
+}
